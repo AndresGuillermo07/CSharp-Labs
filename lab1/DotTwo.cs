18acba66d704f66c4feedfe89ee6da130ad9fa2f
Andres_Aviles <andresaviles0721@gmail.com>
2025-01-10 13:25:39 -0500

    first commit

---
+++ lab1/DotTwo.cs
@@ -0,0 +1,55 @@
+﻿namespace lab1;
+
+public class DotTwo
+{
+
+    // Method to convert int to Short
+    public short IntToShort(int intValue)
+    {
+        return Convert.ToInt16(intValue);
+    }
+    
+    // Method to convert Int to Long
+    public long IntToLong(int intValue)
+    {
+        return Convert.ToInt64(intValue);
+    }
+
+    public int FloatToInt(float floatValue)
+    {
+        return (int) floatValue;
+    }
+
+    public float IntToFloat(int intValue)
+    {
+        return Convert.ToSingle(intValue);
+    }
+
+    public double IntToDouble(int intValue)
+    {
+        return intValue;
+    }
+
+    public string BoolToString(bool boolValue)
+    {
+        return boolValue.ToString();
+    }
+
+    public string IntToString(int intValue)
+    {
+        return intValue.ToString();
+    }
+
+    public object IntToObject(int intValue)
+    {
+        return intValue;
+    }
+
+    public string? ObjectToString(object objectValue)
+    {
+        return objectValue.ToString();
+    }
+
+
+
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace lab1;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        
+        DotTwo dotTwo = new DotTwo();
+        float floatValue = dotTwo.FloatToInt(1.2f);
+        Console.WriteLine(floatValue.GetType());
+        
+        // INT TO SHORT
+        short shortValue = dotTwo.IntToShort(23);
+        Console.WriteLine(shortValue.GetType());
+        
+        // INT TO LONG
+        long longValue = dotTwo.IntToLong(23);
+        Console.WriteLine(longValue.GetType());
+        
+        // INT TO STRING
+        string stringValue = dotTwo.IntToString(23);
+        Console.WriteLine(stringValue.GetType());
+        
+        // ARGUMENTS
+        Console.WriteLine("\n#-#-# ARGS #-#-#");
+        Console.ForegroundColor = ConsoleColor.Green;
+        ReadParammeters(args);
+
+    }
+
+    public static void ReadParammeters(string[] args)
+    {
+        foreach (var arguments in args)
+        {
+            Console.WriteLine(arguments);
+        }
+    }
+}
